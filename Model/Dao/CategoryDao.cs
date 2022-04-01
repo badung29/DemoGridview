855f38ee526232afb405532b6af703a1fe376aea
Nguyen Ba Dung <nbdung@hyperion.co.jp>
2022-04-01 18:40:24 +0700

    show girdView

---
+++ Model/Dao/CategoryDao.cs
@@ -0,0 +1,41 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dao
+{
+    public class CategoryDao
+    {
+        OnlineShopDbContext db = null;
+        public CategoryDao()
+        {
+            db = new OnlineShopDbContext();
+        }
+
+
+        public List<ProductCategory> ListAll()
+        {
+            return db.ProductCategories.Where(x => x.Status == true).ToList();
+        }
+
+        public long Insert(ProductCategory entity)
+        {
+            db.ProductCategories.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        public ProductCategory ViewDetail(long id)
+        {
+            return db.ProductCategories.Find(id);
+        }
+
+        public bool CheckProductCategoryName(string Name)
+        {
+            return db.ProductCategories.Count(x => x.Name == Name) > 0;
+        }
+    }
+}
---
+++ Model/EF/Cart.cs
@@ -0,0 +1,20 @@
+namespace Model.EF
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Cart")]
+    public partial class Cart
+    {
+        public long ID { get; set; }
+
+        public long CustomerID { get; set; }
+
+        public long ProductID { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
