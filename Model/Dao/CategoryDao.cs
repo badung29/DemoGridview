402b540fd42d32e55be1ff1cf316ff49a3279e98
Nguyen Ba Dung <nbdung@hyperion.co.jp>
2022-04-26 09:41:14 +0700

    update chart

--- Model/Dao/CategoryDao.cs
+++ Model/Dao/CategoryDao.cs
@@ -19,17 +19,38 @@ namespace Model.Dao
         {
             return db.ProductCategories.Where(x => x.Status == true).ToList();
         }
-
-        public IEnumerable<PieChartCategory> ListCategory() {
-            IQueryable<PieChartCategory> model = from category in db.ProductCategories
-                                                 where category.ParentID == null
-                                                 select new PieChartCategory()
+        public IEnumerable<ProductViewModel> ListProduct()
+        {
+            IQueryable<ProductViewModel> model = from product in db.Products
+                                                 join productCategory in db.ProductCategories
+                                                 on product.CategoryID equals productCategory.ID into temp
+                                                 from tem in temp.DefaultIfEmpty()
+                                                 select new ProductViewModel()
                                                  {
-                                                     Name = category.Name,
-                                                     Value = (int)category.DisplayOrder
+                                                     CateName = tem.Name != null ? tem.Name : "No Category",
+                                                     ID = product.ID,
+                                                     Name = product.Name,
+                                                     Code = product.Code,
+                                                     MetaTitle = product.MetaTitle,
+                                                     Price = (decimal)product.Price,
+                                                     Quantity = product.Quantity
                                                  };
             return model.ToList();
+        }
 
+        public IEnumerable<PieChartCategory> ListCategory()
+        {
+            var dataProduct = ListProduct();
+            var model = new List<PieChartCategory>();
+            Dictionary<string, int> checkDup = dataProduct.GroupBy(x => x.CateName)
+                                        .Where(g => g.Count() > 1)
+                                        .ToDictionary(x => x.Key, x => x.Count());
+
+            foreach (var datas in checkDup)
+            {
+                model.Add(new PieChartCategory() { Name = datas.Key, Value = datas.Value });
+            }
+            return model.ToList();
         }
     }
 }
---
+++ Model/ViewModel/CategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ViewModel
+{
+    public class CategoryViewModel
+    {   
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public string ProductCode { get; set; }
+    }
+}
